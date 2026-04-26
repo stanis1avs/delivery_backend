6a78619048f7fb174f3324bff7df2da503b3027d
Stanislav <stanislav-zabrodin@outlook.com>
2026-04-26 18:10:39 +0400

    add port for db config

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     dialect: "postgres"
   },
   production: {
@@ -13,6 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     dialect: "postgres"
   }
 };
